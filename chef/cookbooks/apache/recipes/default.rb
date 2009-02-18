319e5c3f4d8fbd6db7eb0ee85ef955be35c7b421
Matthew Landauer <matthew@openaustralia.org>
2009-02-18 18:37:48 +1100

    Moved creating of ssl certificate to phpmyadmin

--- chef/cookbooks/apache/recipes/default.rb
+++ chef/cookbooks/apache/recipes/default.rb
@@ -12,25 +12,6 @@ remote_file "httpd.conf" do
   group "wheel"
 end
 
-# SSL key (first step of self-signed certificate)
-execute "openssl genrsa 1024 > server.key" do
-  cwd "/usr/local/etc/apache22"
-  creates "/usr/local/etc/apache22/server.key"
-end
-
-# Provide defaults for generating certificate so this can all be done automatically
-remote_file "/etc/ssl/openssl.cnf" do
-  source "openssl.cnf"
-  mode 0644
-  owner "root"
-  group "wheel"
-end
-
-execute "openssl req -batch -new -x509 -nodes -sha1 -days 365 -key server.key > server.crt" do
-  cwd "/usr/local/etc/apache22"
-  creates "/usr/local/etc/apache22/server.crt"
-end
-
 service "apache22" do
   supports :status => true, :restart => true, :reload => true
   action [:enable, :start]
--- chef/cookbooks/phpmyadmin/recipes/default.rb
+++ chef/cookbooks/phpmyadmin/recipes/default.rb
@@ -6,6 +6,25 @@ package "phpmyadmin" do
   source "ports"
 end
 
+# SSL key (first step of self-signed certificate)
+execute "openssl genrsa 1024 > server.key" do
+  cwd "/usr/local/etc/apache22"
+  creates "/usr/local/etc/apache22/server.key"
+end
+
+# Provide defaults for generating certificate so this can all be done automatically
+remote_file "/etc/ssl/openssl.cnf" do
+  source "openssl.cnf"
+  mode 0644
+  owner "root"
+  group "wheel"
+end
+
+execute "openssl req -batch -new -x509 -nodes -sha1 -days 365 -key server.key > server.crt" do
+  cwd "/usr/local/etc/apache22"
+  creates "/usr/local/etc/apache22/server.crt"
+end
+
 template "#{@node[:apache][:dir]}/sites-available/secure.openaustralia.org" do
   source "apache.conf.erb"
   mode 0644
