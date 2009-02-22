c11717847b8fb4a07a9c481350730790ed2e01f5
Matthew Landauer <matthew@openaustralia.org>
2009-02-22 17:30:40 +1100

    Extracted attributes and simplified directory structure of jira install and changed default url

--- chef/cookbooks/jira/attributes/jira.rb
+++ chef/cookbooks/jira/attributes/jira.rb
@@ -1,8 +1,7 @@
-jira_virtual_host_name "jira.openaustralia.org" unless attribute?("jira_virtual_host_name")
-jira_virtual_host_alias "jira.openaustralia.org" unless attribute?("jira_virtual_host_alias")
+jira_virtual_host_name "tickets.test-openaustralia.org" unless attribute?("jira_virtual_host_name")
 # type-version-standalone
 jira_version "enterprise-3.13.2" unless attribute?("jira_version")
-jira_install_path "/www/jira.openaustralia.org/jira" unless attribute?("jira_install_path")
+jira_install_path "/www/#{jira_virtual_host_name}" unless attribute?("jira_install_path")
 jira_database "mysql" unless attribute?("jira_database")
 jira_database_name "jiradb" unless attribute?("jira_database_name")
 jira_database_host "localhost" unless attribute?("jira_database_host")
--- chef/cookbooks/jira/recipes/default.rb
+++ chef/cookbooks/jira/recipes/default.rb
@@ -29,49 +29,42 @@
 include_recipe "java"
 include_recipe "apache"
 
-directory "/www/jira.openaustralia.org" do
-  owner "www"
-  group "www"
-end
-
 unless File.exists?(node[:jira_install_path])
+  puts "Downloading and installing jira to #{node[:jira_install_path]}..."
   remote_file "jira" do
     path "/tmp/jira.tar.gz"
     source "http://downloads.atlassian.com/software/jira/downloads/atlassian-jira-#{node[:jira_version]}-standalone.tar.gz"
+    not_if { File.exists?(path) }
   end
   
+  # Hmmm... Making the untar verbose (with the 'v' option) makes this command hang on FreeBSD
   execute "untar-jira" do
-    command "(cd /tmp; tar zxvf /tmp/jira.tar.gz)"
+    command "(cd /tmp; tar zxf /tmp/jira.tar.gz)"
   end
   
   execute "install-jira" do
     command "mv /tmp/atlassian-jira-#{node[:jira_version]}-standalone #{node[:jira_install_path]}"
   end
+
+  # Set permissions on whole directory. Hmmm.. Feels icky.
+  execute "chown -R #{node[:jira_run_user]} #{node[:jira_install_path]}"
 end
 
 unless File.exists?("#{node[:jira_install_path]}/common/lib/mysql-connector-java-5.1.7-bin.jar")
+  puts "Downloading and installing MySQL connector..."
   remote_file "mysql-connector" do
     path "/tmp/mysql-connector.tar.gz"
     source "http://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-5.1.7.tar.gz/from/http://mysql.mirrors.ilisys.com.au/"
+    not_if { File.exists?(path) }
   end
 
   execute "untar-mysql-connector" do
-    command "(cd /tmp; tar zxvf /tmp/mysql-connector.tar.gz)"
+    command "(cd /tmp; tar zxf /tmp/mysql-connector.tar.gz)"
   end
 
   execute "install-mysql-connector" do
     command "cp /tmp/mysql-connector-java-5.1.7/mysql-connector-java-5.1.7-bin.jar #{node[:jira_install_path]}/common/lib"
   end
-end
-
-directory "/www/jira.openaustralia.org/jira" do
-  owner "www"
-  group "www"
-end
-
-directory "/www/jira.openaustralia.org/jira/logs" do
-  owner "www"
-  group "www"
 end
 
 template "#{node[:jira_install_path]}/conf/server.xml" do
@@ -89,13 +82,13 @@ end
 apache_module "proxy"
 apache_module "proxy_http"
 
-template "#{node[:apache][:dir]}/sites-available/jira.openaustralia.org" do
+template "#{node[:apache][:dir]}/sites-available/#{node[:jira_virtual_host_name]}" do
   source "apache.conf.erb"
   owner "root"
   mode 0644
 end
 
-apache_site "jira.openaustralia.org"
+apache_site node[:jira_virtual_host_name]
 
 template "/usr/local/etc/rc.d/jira" do
   source "jira.erb"
