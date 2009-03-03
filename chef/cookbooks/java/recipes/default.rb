378d7f49cb8bf298f169a806c1e638378e690d24
Matthew Landauer <matthew@openaustralia.org>
2009-03-03 15:39:19 +1100

    Removed installation of pre-requisite for installing java as this will become obvious when trying to install the java package by hand

--- chef/cookbooks/java/recipes/default.rb
+++ chef/cookbooks/java/recipes/default.rb
@@ -1,5 +1,3 @@
-package "javavmwrapper"
-
 # Installation of java needs to be actually be done by hand because a license agreement needs to be agreed
 # Go to http://www.freebsdfoundation.org/downloads/java.shtml and download diablo-jdk-freebsd7.i386.1.6.0.07.02.tbz
 # and install it with "pkg_add diablo-jdk-freebsd7.i386.1.6.0.07.02.tbz"
