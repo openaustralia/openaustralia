250687f8bdc42fb5bc920b4436b6e81d092c0856
Matthew Landauer <matthew@openaustralia.org>
2009-03-08 20:03:08 +1100

    Handle situation when php extensions file doesn't yet exist

--- chef/cookbooks/xapian/definitions/remote_file_line.rb
+++ chef/cookbooks/xapian/definitions/remote_file_line.rb
@@ -1,5 +1,7 @@
 # Only add a line if it is absent from a file
 define :remote_file_line do
+  # If the file doesn't exist, first create it
+  file params[:name]
   execute "echo '#{params[:line]}' >> #{params[:name]}" do
     not_if { File.open(params[:name]).find { |line| line.strip == params[:line] } }
   end
