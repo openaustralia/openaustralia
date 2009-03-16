3c65667cc810189114162d5742c2df34d0ecfdf2
Matthew Landauer <matthew@openaustralia.org>
2009-03-16 17:37:02 +1100

    Added cookbook for git-daemon

---
+++ chef/cookbooks/git-daemon/recipes/default.rb
@@ -0,0 +1,24 @@
+require_recipe "git"
+
+# Add some options to /etc/rc.conf for the git-daemon
+# TODO: Need to restart the daemon anytime we change these options
+remote_file_line "/etc/rc.conf" do
+  line 'git_daemon_directory="/home/git/repositories"'
+end
+
+remote_file_line "/etc/rc.conf" do
+  line 'git_daemon_flags="--syslog --base-path=/home/git/repositories"'
+end
+
+remote_file_line "/etc/rc.conf" do
+  line 'git_daemon_user="git"'
+end
+
+service "git_daemon" do
+  supports :status => true
+  action [:enable, :start]
+end
+
+# To enable a repository to be exported by git_daemon must touch a file called git-daemon-export-ok in the
+# root directory of the repository.
+# For instance: touch /home/git/repositories/openaustralia.git/git-daemon-export-ok
