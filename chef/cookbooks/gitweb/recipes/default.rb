452ec131049ba57f0ad21e94a2a28dd503d540fd
Matthew Landauer <matthew@openaustralia.org>
2009-03-17 12:29:20 +1100

    Bunch of fixed to actually deploy gitweb on the production server

--- chef/cookbooks/gitweb/recipes/default.rb
+++ chef/cookbooks/gitweb/recipes/default.rb
@@ -1,26 +1,53 @@
 require_recipe "apache"
 
 unless File.exists?("#{node[:gitweb_root]}/gitweb.cgi")
+  # This is a HACK
+  workdir_prefix = "/tmp"
+  workdir = "#{workdir_prefix}/usr/ports/devel/git/work/git-1.6.0.6"
+  
   execute "make configure" do
     cwd "/usr/ports/devel/git"
-    creates "/usr/ports/devel/git/work"
+    creates workdir
+    # Specifically setting WRKDIRPREFIX as a temporary workaround for bug in run_command which
+    # is not passing on environment from parent
+    environment "WRKDIRPREFIX" => workdir_prefix
   end
 
   execute "gmake gitweb/gitweb.cgi" do
-    cwd '/usr/ports/devel/git/work/git-1.6.1.3'
-    creates "/usr/ports/devel/git/work/git-1.6.1.3/gitweb/gitweb.cgi"
+    cwd "#{workdir}"
+    creates "#{workdir}/gitweb/gitweb.cgi"
   end
 
-  directory node[:gitweb_root]
-
+  # Fix path on the cgi script. On the server perl is only available in /usr/local/bin
+  execute "sed 's$/usr/bin/perl$/usr/local/bin/perl$' gitweb.cgi > gitweb.cgi.fixed.path" do
+    cwd "#{workdir}/gitweb"
+    creates "#{workdir}/gitweb/gitweb.cgi.fixed.path"
+  end
+  
+  execute "mv gitweb.cgi.fixed.path gitweb.cgi" do
+    cwd "#{workdir}/gitweb"
+  end
+  
+  directory node[:gitweb_root] do
+    owner "www"
+    group "www"
+  end
+  
   execute "cp git-favicon.png git-logo.png gitweb.cgi gitweb.css #{node[:gitweb_root]}" do
-    cwd "/usr/ports/devel/git/work/git-1.6.1.3/gitweb"
+    cwd "#{workdir}/gitweb"
     creates "#{node[:gitweb_root]}/gitweb.cgi"
   end
-
+  
+  # The cgi script needs to be executable
+  file "#{node[:gitweb_root]}/gitweb.cgi" do
+    mode 0755
+  end
+  
   execute "make clean" do
     cwd "/usr/ports/devel/git"
+    environment "WRKDIRPREFIX" => workdir_prefix
   end
+  
 end
 
 # The configuration file for gitweb
