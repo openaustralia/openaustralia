df948737936e1258d6a3f62425ef065d79400125
Brenda Wallace <brenda@wallace.net.nz>
2026-03-09 08:50:56 +1300

    safety rails

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,5 @@
+# FOR SAFETY this is commented out until we have approved the staging deployment
+# this prevents accidental deployment to production
 # server 'openaustralia.org.au', user: 'deploy', roles: %w[app web], primary: true
 
 # set :deploy_to, '/srv/www/production'
