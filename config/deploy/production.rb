fb9a149974e44f2dad7720f98d0363704f88f86d
Brenda Wallace <brenda@wallace.net.nz>
2026-04-13 11:28:20 +1200

    change server name to newprod

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
-puts '=' * 75, 'NOTICE: using branch: staging on server: staging.openaustralia.org.au NOT production!', '=' * 75
+puts '=' * 75, 'NOTICE: using branch: staging on server: newprod.openaustralia.org.au NOT production!', '=' * 75
 
-server 'staging.openaustralia.org.au', user: 'deploy', roles: %w[app web], primary: true
+server 'newprod.openaustralia.org.au', user: 'deploy', roles: %w[app web], primary: true
 
 set :deploy_to, '/srv/www/production'
 set :branch, 'staging'
